2054254ef2f388e526a8455ca7e8b5a848d895b6
Velitheslav <102607561+Velitheslav@users.noreply.github.com>
2022-11-09 10:23:20 +0100

    Update Wheel_and_Pedals.ino

--- CODE/Wheel_and_Pedals/Wheel_and_Pedals.ino
+++ CODE/Wheel_and_Pedals/Wheel_and_Pedals.ino
@@ -6,8 +6,8 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD, 3, 0, true,
 
 const int potPin = A0; //pin A0 to read analog input
 const int potPin2 = A1;
-const int potPin3 = A2;
-const int but1=A5;
+const int potPin3 = A2;r
+const int but1=2;
 
 //Variables:
 int value; //save analog value
@@ -28,13 +28,20 @@ void setup(){
 }
 
 void loop(){
-
+//Joystick.setButton(button1, 0);
  
   Serial.println("****new loop****");
   value = analogRead(potPin);   //Read and save analog value from potentiometer
   value2 = analogRead(potPin2);
   value3 = analogRead(potPin3);
-  button1 = digitalRead(but1);
+  
+    button1 = digitalRead(but1); 
+    //Joystick.setButton(but1, 1);   
+    Serial.println(but1);
+    Serial.println(button1);
+    
+  
+
 
   //value = map(value, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
   //analogWrite(ledPin, value); //Send PWM value to led
@@ -52,23 +59,55 @@ void loop(){
   Joystick.setYAxis(value2/2);
 
   Joystick.setZAxis(value3);
-
+/*
   //Joytick.setBuuton();
  
 
- 
+            switch (button1) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
+                   
+                    case 1:
+                              Joystick.setButton(button1, 1);
+                              Serial.println(button1);
+                              break;
+                    
+                    case 0:
+                              Joystick.setButton(button1, 0);
+                              Serial.println(button1);
+                              break;
+            }
+
+
+//Joystick.setButton(button1, 1);
+ */
+ /*
   if (button1 == HIGH) {
     //if(oldState==LOW){
       Joystick.setButton(button1, 32);
-      Serial.println("pressed");
-      oldState=HIGH;
+      Serial.println(button1);
+      button1=LOW;
+      Joystick.setButton(button1, 0);
+      //oldState=HIGH;
     //}
     
   } else  {
     //if(oldState==HIGH){
       Joystick.setButton(button1, 0);
-      Serial.println("almost pressed");
-      oldState=LOW;
+      Serial.println(button1);
+      //oldState=LOW;
     //}
    }
+   //delay(100);
+*/
+/*
+if(button1==LOW){
+  Joystick.setButton(button1, 0);
+  
+}
+else{
+  Joystick.setButton(button1, 1);
+  delay(5000);
+  //Joystick.setButton(button1, 0);
+}
+Joystick.setButton(button1, 0);
+*/
 }
